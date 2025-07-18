fd8f091417e4427be7e268850f56532438ba96f6
Ekaterina <katusha158@rambler.ru>
2025-07-18 23:07:52 +0300

    com_3: создали папки env, и вынесли секретный ключ

--- milksite/settings.py
+++ milksite/settings.py
@@ -11,6 +11,12 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+# установим python dotenv и импортируем
+from dotenv import load_dotenv
+import os
+
+# загрузили переменные окружения
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3i3w(hdcmcm2$+=v7k-fvm83@ab!_npx%cp-j$003v4uu9+)#%'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
