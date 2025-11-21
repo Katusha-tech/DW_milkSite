010bb720a11ed2f0c47dfe973f974b3d5a8145e1
Ekaterina <katusha158@rambler.ru>
2025-11-21 06:48:46 +0300

    com_29: изменила файл setting.py database для переноса данных в postgresql, установила драйвер в терминале для работы с PostgreSql, сделала миграции

--- milksite/settings.py
+++ milksite/settings.py
@@ -93,8 +93,12 @@ WSGI_APPLICATION = 'milksite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'milkdb',
+        'USER': 'milkuser',
+        'PASSWORD': 'Strong123',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
